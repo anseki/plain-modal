cc3d136ac03c34c73dbc4cff817448d07f4587a8
anseki <AnSeki.aff@gmail.com>
2018-06-22 12:13:32 +0900

    v1.0.19

--- test/plain-modal-limit.js
+++ test/plain-modal-limit.js
@@ -2052,10 +2052,11 @@ function abort(props) {
 
 /**
  * @param {props} props - `props` of instance.
- * @param {boolean} [force] - Skip transition.
+ * @param {boolean} force - Skip transition.
+ * @param {Array} args - Arguments that are passed to procToOn.
  * @returns {void}
  */
-function _on(props, force) {
+function _on(props, force, args) {
   if (props.isOn && props.state === STATE_STOPPED || props.isOn && props.state !== STATE_STOPPED && !force) {
     return;
   }
@@ -2066,7 +2067,8 @@ function _on(props, force) {
   */
 
   if (props.options.procToOn) {
-    props.options.procToOn.call(props.ins, !!force);
+    args.unshift(!!force);
+    props.options.procToOn.apply(props.ins, args);
   }
 
   if (force || !props.isOn && props.state === STATE_DELAYING || -props.delay > props.duration) {
@@ -2100,10 +2102,11 @@ function _on(props, force) {
 
 /**
  * @param {props} props - `props` of instance.
- * @param {boolean} [force] - Skip transition.
+ * @param {boolean} force - Skip transition.
+ * @param {Array} args - Arguments that are passed to procToOff.
  * @returns {void}
  */
-function _off(props, force) {
+function _off(props, force, args) {
   if (!props.isOn && props.state === STATE_STOPPED || !props.isOn && props.state !== STATE_STOPPED && !force) {
     return;
   }
@@ -2114,7 +2117,8 @@ function _off(props, force) {
   */
 
   if (props.options.procToOff) {
-    props.options.procToOff.call(props.ins, !!force);
+    args.unshift(!!force);
+    props.options.procToOff.apply(props.ins, args);
   }
 
   if (force || props.isOn && props.state === STATE_DELAYING || -props.delay > props.duration) {
@@ -2277,6 +2281,7 @@ var TimedTransition = function () {
      * Set `on`.
      * @param {boolean} [force] - Set `on` it immediately without transition.
      * @param {Object} [options] - New options.
+     * @param {...{}} [args] - Arguments that are passed to procToOn.
      * @returns {TimedTransition} Current instance itself.
      */
 
@@ -2289,20 +2294,28 @@ var TimedTransition = function () {
       }
 
       this.setOptions(options);
-      _on(insProps[this._id], force);
+      _on(insProps[this._id], force, Array.prototype.slice.call(arguments, 2));
       return this;
     }
 
     /**
      * Set 'off'.
      * @param {boolean} [force] - Set `off` it immediately without transition.
+     * @param {Object} [options] - New options.
+     * @param {...{}} [args] - Arguments that are passed to procToOff.
      * @returns {TimedTransition} Current instance itself.
      */
 
   }, {
     key: 'off',
-    value: function off(force) {
-      _off(insProps[this._id], force);
+    value: function off(force, options) {
+      if (arguments.length < 2 && typeof force !== 'boolean') {
+        options = force;
+        force = false;
+      }
+
+      this.setOptions(options);
+      _off(insProps[this._id], force, Array.prototype.slice.call(arguments, 2));
       return this;
     }
   }, {
